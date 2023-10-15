f0e58daddc7eb0c563d303f6b3d32be5b460ebbe
Shamil Umaev <shamil.umaev@hotmail.com>
2023-10-15 21:24:54 +0200

    Submit and display data

--- script.js
+++ script.js
@@ -10,4 +10,50 @@ const getData = async (ipAddress) => {
     return data;
 }
 
-getData('8.8.8.8');
+const regexCheck = (input) => {
+    const ipAddressRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.([25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.([25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.([25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipAddressRegex.test(input);
+}
+
+const inputCheck = () => {
+
+}
+
+const displayData = async () => {
+    const data = await getData(input.value);
+    const locationInfoCard = document.createElement('div');
+    locationInfoCard.classList.add('location-info-card');
+    locationInfoCard.innerHTML = `
+        <div class="ip-address-ctr">
+            <p class="label">Ip Address</p>
+            <h4>${data.ip}</h4>
+        </div>
+        <div class="location-ctr">
+            <p class="label">Location</p>
+            <h4>${data.location.city}, ${data.location.region}</h4>
+        </div>
+        <div class="timezone-ctr">
+            <p class="label">Timezone</p>
+            <h4>UTC ${data.location.timezone}</h4>
+        </div>
+        <div class="isp-ctr">
+            <p class="label">ISP</p>
+            <h4>${data.isp}</h4>
+        </div>
+    `;
+
+    headerTop.appendChild(locationInfoCard);
+    input.value = '';
+}
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    if(input.value !== '' && regexCheck(input.value)) {
+        displayData();
+    } else {
+        console.log('Error');
+    }
+}
+
+form.addEventListener('submit', onSubmit);
+// window.addEventListener('DOMContentLoaded', displayData);
